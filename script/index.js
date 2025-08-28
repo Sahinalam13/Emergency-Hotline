c16c5e33515ba80e41f7f2a401b5207b87d2acaa
Sahinalam13 <mohon20022@gmail.com>
2025-08-28 16:12:26 +0600

    clear and copy functionalities add

--- script/index.js
+++ script/index.js
@@ -2,9 +2,9 @@
 const hearts = document.getElementsByClassName("heart-icon")
 for(let heart of hearts){
     heart.addEventListener('click',function(){
-   const hearCount = document.getElementById("heart-add").innerText
+   const hearCount = document.getElementById("heart-count").innerText
    const currentHeartCount = Number(hearCount) + 1;
-   document.getElementById("heart-add").innerText= currentHeartCount;
+   document.getElementById("heart-count").innerText= currentHeartCount;
  
     })
 }
@@ -15,15 +15,15 @@ for(let callbtn of callBtns){
        const serviceName = callbtn.parentNode.parentNode.children[1].children[1].innerText;
        const serviceNumber = callbtn.parentNode.parentNode.children[2].children[0].innerText;
        let time = new Date().toLocaleTimeString("en-US");
-       let coins = parseInt(document.getElementById("coins").innerText);
+       let coins = parseInt(document.getElementById("coins-count").innerText);
        totalCoins = coins;
     let newCoins = coins - 20;
     if(newCoins < 20){
-        newCoins = 20;
+
         alert("Not Enough Coins To Make A Call")
-        return
+        return;
        }
-       document.getElementById("coins").innerText= newCoins
+       document.getElementById("coins-count").innerText= newCoins
        
        
        alert(`Calling ${serviceName} at ${serviceNumber}... `)
@@ -42,6 +42,39 @@ for(let callbtn of callBtns){
        document.getElementById("history").appendChild(historyDiv)
 
     
-    } )
+    })
 }
+
+// clear button functionalities
+const clearBtn = document.getElementById("clear-btn");
+clearBtn.addEventListener("click", function(){
+    document.getElementById("history").innerHTML="";
+})
+
+// copy button functionalities
+const copyBtns = document.getElementsByClassName('copy-btn')
+for(let copybtn of copyBtns){
+    copybtn.addEventListener('click', function(){
+        
+        const card  = copybtn.closest('.card');
+        const number = card.querySelector('span').innerText;
+        
+        navigator.clipboard.writeText(number)
+        .then(() =>{
+
+        const copyCount = document.getElementById("copy-count").innerText;
+        const currentCopyCount = Number(copyCount) + 1;
+
+        document.getElementById("copy-count").innerText = currentCopyCount;
+
+        alert(`Copied: ${number}`)
+        })
+        .catch(err => {
+            console.error("Failed to copy:", err);
+        });
+
+    });
+}
+
+
 
